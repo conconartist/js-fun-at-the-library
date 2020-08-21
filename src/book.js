e38e6eca1b0eefec02513c6cedf1edbc0db82702
Chung Hong <conconart.info@gmail.com>
2020-08-20 20:10:57 -0600

    Update testing results for book

--- src/book.js
+++ src/book.js
@@ -1,12 +1,43 @@
-function createTitle() {
-  
+function createTitle(bookTitle) {
+  return "The " + bookTitle;
+}
+
+function buildMainCharacter() {
+  var ghoulCharacter = {name: "Vassya", age: 16, pronouns: "she/her"};
+  return ghoulCharacter;
+}
+
+function saveReview(newReview, reviews) {
+  if (!reviews.includes(newReview)) {
+    reviews.push(newReview);
+  }
+  return reviews;
+}
+
+function calculatePageCount(bookTitle) {
+  return (bookTitle.length * 20);
+}
+
+function writeBook(bookTitle, bookCharacter, genre){
+  var book = {
+    title: bookTitle,
+    mainCharacter: bookCharacter,
+    pageCount: calculatePageCount(bookTitle),
+    genre: genre,
+  }
+  return book;
+}
+
+function editBook(bookTitle) {
+  bookTitle.pageCount = bookTitle.pageCount * 0.75;
+  return bookTitle.pageCount;
 }
 
 module.exports = {
   createTitle: createTitle,
-  // buildMainCharacter: buildMainCharacter,
-  // saveReview: saveReview,
-  // calculatePageCount: calculatePageCount,
-  // writeBook: writeBook,
-  // editBook: editBook
+  buildMainCharacter: buildMainCharacter,
+  saveReview: saveReview,
+  calculatePageCount: calculatePageCount,
+  writeBook: writeBook,
+  editBook: editBook
 }
